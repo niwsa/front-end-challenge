cce832ae8a5e96e1279f4097ca62ba496bae45aa
Aswin V <vaswin91@gmail.com>
2021-01-07 00:49:05 +0530

    Home page table layout

---
+++ components/svg/UserIcon.js
@@ -0,0 +1,12 @@
+export default function UserIcon(props) {
+  return (
+    <svg viewBox="0 0 24 24" fill="none" {...props}>
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M1.464 15.464A5 5 0 015 14h8a5 5 0 015 5v2a1 1 0 11-2 0v-2a3 3 0 00-3-3H5a3 3 0 00-3 3v2a1 1 0 11-2 0v-2a5 5 0 011.464-3.536zM9 4a3 3 0 100 6 3 3 0 000-6zM4 7a5 5 0 1110 0A5 5 0 014 7z"
+        fill="#fff"
+      />
+    </svg>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,80 @@
+import Shell from "components/Shell";
+import UserIcon from "components/svg/UserIcon";
 import Head from "next/head";
-import styles from "styles/App.module.css";
+import styles from "styles/Home.module.css";
 
-export default function App() {
+export default function Home() {
   return (
-    <div className="flex col h100">
-      <header className={`flex ${styles.header}`}>
-        <div className={`inl-flex al-ctr ml-auto ${styles.userCard}`}>
-          <div className={styles.avatar}></div>
-          <div>
-            <div>Julie Howard </div>
-            <div className="meta">Admin</div>
-          </div>
-        </div>
+    <Shell>
+      <Head>
+        <title>People</title>
+      </Head>
+      <header className="flex al-base">
+        <h2 className={styles.title}>People</h2>
+        <span className="meta no-shrink">3 employees</span>
+        <button type="button" className={`inl-flex al-ctr ${styles.addBtn}`}>
+          <UserIcon aria-hidden="true" width="1rem" height="1rem" />
+          <span>Add employee</span>
+        </button>
       </header>
-      <main className={`h100 scroll ${styles.main}`}></main>
-    </div>
+      <table className={styles.table}>
+        <thead className={`meta ${styles.thead}`}>
+          <tr>
+            <th>EMPLOYEE</th>
+            <th>JOB TITLE</th>
+            <th>COUNTRY</th>
+            <th>SALARY</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody className={styles.tbody}>
+          <tr>
+            <td className={styles.name}>
+              Ann Henry <div className="meta">04/12/1990</div>
+            </td>
+            <td>Product Manager</td>
+            <td>United States</td>
+            <td className={styles.sal}>
+              60,000 USD <span className="meta">per year</span>
+            </td>
+            <td>
+              <button type="button" className="btnSec">
+                Edit
+              </button>
+            </td>
+          </tr>
+          <tr>
+            <td className={styles.name}>
+              Annette Williamson <div className="meta">20/04/1985</div>
+            </td>
+            <td>CEO</td>
+            <td>United States</td>
+            <td className={styles.sal}>
+              60,000 USD <span className="meta">per year</span>
+            </td>
+            <td>
+              <button type="button" className="btnSec">
+                Edit
+              </button>
+            </td>
+          </tr>
+          <tr>
+            <td className={styles.name}>
+              Philip Alexander <div className="meta">25/07/1989</div>
+            </td>
+            <td>Software engineer</td>
+            <td>United States</td>
+            <td className={styles.sal}>
+              60,000 USD <span className="meta">per year</span>
+            </td>
+            <td>
+              <button type="button" className="btnSec">
+                Edit
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </Shell>
   );
 }
