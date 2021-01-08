719835c986ec9036fc2bed078068b84561f9dbf5
Aswin V <vaswin91@gmail.com>
2021-01-09 00:25:48 +0530

    Edit mode for employee

--- pages/employee/[[...id]].js
+++ pages/employee/[[...id]].js
@@ -1,14 +1,44 @@
-import { useState } from "react";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import Shell from "components/Shell";
 import styles from "styles/Employee.module.css";
-import { addOrUpdateEmployeeRecord } from "lib/api";
+import { addOrUpdateEmployeeRecord, loadEmplRecord } from "lib/api";
 
 export default function Employee() {
-  const [formState, setFormState] = useState({});
+  const router = useRouter();
+  const { id: empId } = router.query;
+
+  //  IS EDIT MODE
+  const editMode = !!empId?.[0]; // check if an id is passed via path param
+
+  // FETCH CUR RECORD OF EMPLOYEE TO EDIT IN EDIT MODE
+
+  async function loadEmployee(empId) {
+    const record = await loadEmplRecord(empId);
+    if (record) {
+      setFormState(record);
+    }
+  }
+
+  useEffect(() => {
+    if (editMode) {
+      loadEmployee(empId[0]);
+    }
+  }, [editMode, empId]);
+
+  // FORM LOGIC
+  const formInitialState = {
+    name: "",
+    dob: "",
+    jobTitle: "",
+    country: "",
+    salary: "",
+  };
+  const [formState, setFormState] = useState(formInitialState);
 
   function handleChange(e) {
     const {
-      target: { name, type, value },
+      target: { name, value },
     } = e;
     setFormState((curState) => ({ ...curState, [name]: value }));
   }
@@ -16,28 +46,34 @@ export default function Employee() {
   function onSubmit(e) {
     e.preventDefault();
     addOrUpdateEmployeeRecord(formState);
+    router.push("/");
   }
 
   return (
     <Shell>
-      <section className={styles.container}>
+      <section className={`h100 ${styles.container}`}>
         <header className={styles.header}>
-          <h2 className={styles.title}>Add a new employee</h2>
+          <h2 className={styles.title}>
+            {editMode ? "Edit employee" : "Add a new employee"}
+          </h2>
           <span className="meta">
-            Fill out the information of your new employee.
+            {editMode
+              ? "Edit the information of your employee"
+              : "Fill out the information of your new employee."}
           </span>
         </header>
-        <form className={styles.form} onSubmit={onSubmit}>
+        <form className={`${styles.form}`} onSubmit={onSubmit}>
           <div>
             <div className="flex col">
               <label htmlFor="name">Name</label>
               <input
                 type="text"
+                required
                 id="name"
                 name="name"
                 placeholder="e.g. Jane Doe"
                 autoComplete="off"
-                value={formState.name || ""}
+                value={formState.name}
                 onChange={handleChange}
               ></input>
               <div className="meta">First and last names</div>
@@ -46,11 +82,12 @@ export default function Employee() {
               <label htmlFor="dob">Birthdate</label>
               <input
                 type="text"
+                required
                 id="dob"
                 name="dob"
                 placeholder="e.g. 17/02/1990"
                 autoComplete="off"
-                value={formState.dob || ""}
+                value={formState.dob}
                 onChange={handleChange}
               ></input>
               <div className="meta">DD/MM/YYYY</div>
@@ -59,11 +96,12 @@ export default function Employee() {
               <label htmlFor="jobTitle">Job title</label>
               <input
                 type="text"
+                required
                 id="jobTitle"
                 name="jobTitle"
                 placeholder="e.g. Product manager"
                 autoComplete="off"
-                value={formState.jobTitle || ""}
+                value={formState.jobTitle}
                 onChange={handleChange}
               ></input>
               <div className="meta">What is their role?</div>
@@ -74,15 +112,19 @@ export default function Employee() {
               </label>
               <div className="select">
                 <select
+                  required
                   id="country"
                   name="country"
-                  value={formState.country || ""}
+                  value={formState.country}
                   onChange={handleChange}
                 >
                   <option value="" disabled>
                     Select a country
                   </option>
                   <option value="Portugal">Portugal</option>
+                  <option value="United Kingdom">United Kingdom</option>
+                  <option value="United States">United States</option>
+                  <option value="India">India</option>
                 </select>
               </div>
               <div className="meta">Where are they based?</div>
@@ -91,21 +133,26 @@ export default function Employee() {
               <label htmlFor="salary">Salary</label>
               <input
                 type="number"
+                required
                 id="salary"
                 name="salary"
                 placeholder="e.g. 50000"
                 autoComplete="off"
-                value={formState.salary || ""}
+                value={formState.salary}
                 onChange={handleChange}
               ></input>
               <div className="meta">Gross yearly salary</div>
             </div>
           </div>
           <div className={`flex al-ctr just-ctr ${styles.ctrls}`}>
-            <button type="button" className="btnSec">
+            <button
+              type="button"
+              className="btnSec"
+              onClick={() => router.push("/")}
+            >
               Cancel
             </button>
-            <button type="submit">Add employee</button>
+            <button type="submit">{editMode ? "Save" : "Add employee"}</button>
           </div>
         </form>
       </section>
